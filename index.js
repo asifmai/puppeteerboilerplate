e52b1c8176a61f8c19a09620ca4389f24988ed82
iqbal <asifmai@hotmail.com>
2019-06-18 14:12:10 +0500

    'update'

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
 const puppeteer = require('puppeteer');
 const testURL = 'https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html';
-const pageURL = 'https://www.lowes.com/pd/DEWALT-5-Tool-20-Volt-Max-Power-Tool-Combo-Kit-with-Soft-Case-Charger-Included-and-2-Batteries-Included/3441520';
+const moment = require('moment');
+const fs = require('fs');
 
-(async () => {
+const runBot = async () => {
   try {
+    console.log(dt(), 'Scrape Started');
+
     // Launch Browser
-    const browser = await puppeteer.launch({
-      headless: false,                        // To run on headless: true
-      args: [
-        '--disable-setuid-sandbox',
-        '--disable-infobars',
-        '--window-position=0,0',
-        '--ignore-certifcate-errors',
-        '--ignore-certifcate-errors-spki-list',
-        '--window-size=1366,768',      
-        // ['--no-sandbox'],                  // To run on linux
-        '--proxy-server=143.255.52.90:8080',    //To use a sock5 proxy
-        // '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
-      ],
-      ignoreHTTPSErrors: true,
-      userDataDir: './tmp',
-    });
+    const browser = await launchBrowser();
 
     // Launch New Page
-    const page = await createPage(browser);
+    const page = await launchPage(browser);
 
     // Goto the URL
-    const response = await page.goto(pageURL, {
+    const response = await page.goto(testURL, {
       timeout: 0,
       waitUntil: 'load'
     });
 
-    console.log(response.status());
+    console.log(dt(), 'Page Response Status: ', response.status(), response.statusText());
     if (response.status() === 403) { // If IP is blocked
-      console.log('Your ip Blocked by Website...');
+      console.log(dt(), 'Your ip Blocked by Website...');
     } else if (response.status() === 404) { // If Page Not found
-      console.log('Page Not Found...');
+      console.log(dt(), 'Page Not Found...');
     } else { // Everything is good
       // Your
       // Code
@@ -51,14 +39,15 @@ const pageURL = 'https://www.lowes.com/pd/DEWALT-5-Tool-20-Volt-Max-Power-Tool-C
 
     // Close the browser
     await browser.close();
+    console.log(dt(), 'Scrape Completed');
     return 'Completed...';
   } catch (error) {
+    console.log(dt(), 'Scraping Error: ', error)
     return error;
   }
-})();
+};
 
-
-const createPage = (browser) => new Promise(async (resolve, reject) => {
+const launchPage = (browser) => new Promise(async (resolve, reject) => {
   try {
     // Create New Page
     const page = await browser.newPage();
@@ -100,8 +89,42 @@ const createPage = (browser) => new Promise(async (resolve, reject) => {
 
     // Set Extra Header for request
     // await page.setExtraHTTPHeaders({'iqbal': 'Pakistan'});
+    console.log(dt(), 'Launched Page');
     resolve(page);
   } catch (error) {
+    console.log(dt(), 'Launch Page Error: ', error)
     reject(error);
   }
 });
+
+const launchBrowser = () => new Promise(async (resolve, reject) => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: false,                        // To run on headless: true
+      args: [
+        '--disable-setuid-sandbox',
+        '--disable-infobars',
+        '--window-position=0,0',
+        '--ignore-certifcate-errors',
+        '--ignore-certifcate-errors-spki-list',
+        '--window-size=1366,768',      
+        // ['--no-sandbox'],                  // To run on linux
+        '--proxy-server=143.255.52.90:8080',    //To use a sock5 proxy
+        // '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
+      ],
+      ignoreHTTPSErrors: true,
+      userDataDir: './tmp',
+    });
+    console.log(dt(), 'Launched Browser');
+    resolve(browser);
+  } catch (error) {
+    console.log(dt(), 'Browser Launch Error: ', error);
+    reject(error);
+  }
+});
+
+const dt = () => {
+  return moment().format('YYYY-MM-DD HH:mm:ss') + ' -';
+}
+
+runBot();
